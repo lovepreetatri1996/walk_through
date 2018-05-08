cc925c67b310420c6a5811e7ebffdc0e8b43ade4
lovepreetatri1996 <lovepreetatri2405@gmail.com>
2018-05-08 17:10:47 +0530

    first commit

---
+++ app/src/main/java/com/demotxt/droidsrce/slide/MainActivity.java
@@ -0,0 +1,24 @@
+package com.demotxt.droidsrce.slide;
+
+import android.content.Intent;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private ViewPager viewPager;
+    private SlideAdapter myadapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        myadapter = new SlideAdapter(this);
+        viewPager.setAdapter(myadapter);
+
+    }
+    public void  skip(View view){
+        finish();
+    }
+}
